b7f5179ce3cb306c31c63648f1005c883b0bb4ab
Omer <omerfarukdasar@gmail.com>
2022-04-18 16:37:55 +0200

    updateUser

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,7 +1,9 @@
 // adding function to data base
 import firebase from "./firebase";
-import { getDatabase, push, ref, set } from "firebase/database";
+import { getDatabase, onValue, push, ref, set } from "firebase/database";
+import { useEffect, useState } from "react";
 
+// adding info
 export const AddUser = (info) => {
   const db = getDatabase();
   const userRef = ref(db, "clientInformation");
@@ -11,4 +13,27 @@ export const AddUser = (info) => {
     phoneNumber: info.phoneNumber,
     gender: info.gender,
   });
+};
+
+export const useFetch = () => {
+  const [isLoading, setIsLoading] = useState();
+  const [contactList, setContactList] = useState();
+
+  useEffect(() => {
+    setIsLoading(true);
+    const db = getDatabase();
+    const userRef = ref(db, "clientInformation");
+
+    onValue(userRef, (snapshot) => {
+      const data = snapshot.val();
+      const infoArray = [];
+
+      for (let id in data) {
+        infoArray.push({ id, ...data[id] });
+      }
+      setContactList(infoArray);
+      setIsLoading(false);
+    });
+  }, []);
+  return { isLoading, contactList };
 };
